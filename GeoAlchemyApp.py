3c5a028998d6c8a3f5ada85cea642203b2773238
celikn <naimecelik@gmail.com>
2016-03-25 14:30:53 +0200

    Point coordinates from db assinged as popup via jinja2

--- GeoAlchemyApp.py
+++ GeoAlchemyApp.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 from sqlalchemy import (create_engine, MetaData, Column, Integer, String,func, select)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,7 +11,7 @@ from geoalchemy2.functions import GenericFunction
 ### EXAMPLE with Leaflet
 appgeo = Flask(__name__, template_folder="templates")
 
-engine = create_engine('postgresql://postgres:postgres@localhost/flaskgisdb', echo=False)
+engine = create_engine('postgresql://postgres:postgres@localhost/flaskgisdb', echo=True)
 #"'postgresql://username:password@localhost/databasename'
 metadata = MetaData(engine)
 Base = declarative_base(metadata=metadata)
@@ -33,40 +35,27 @@ class Road(Base):
         name = Column(String)
         geom = Column(Geometry('LINESTRING'))
 
+#
+#
+# metadata.drop_all()
+# metadata.create_all()
+#
 
-
-metadata.drop_all()
-metadata.create_all()
 
 
 session = sessionmaker(bind=engine)()
 # Add objects.  We can use strings...
-session.add_all([
-         Road(name='Jeff Rd', geom='LINESTRING(191232 243118,191108 243242)'),
-         Road(name='Geordie Rd', geom='LINESTRING(189141 244158,189265 244817)'),
-         Road(name='Paul St', geom='LINESTRING(192783 228138,192612 229814)'),
-         Road(name='Graeme Ave', geom='LINESTRING(189412 252431,189631 259122)'),
-         Road(name='Phil Tce', geom='LINESTRING(190131 224148,190871 228134)'),
-     ])
-
-
-
-
-
-session.add_all([
-         Road(name='Jeff Rd', geom='LINESTRING(191232 243118,191108 243242)'),
-         Road(name='Geordie Rd', geom='LINESTRING(189141 244158,189265 244817)'),
-         Road(name='Paul St', geom='LINESTRING(192783 228138,192612 229814)'),
-         Road(name='Graeme Ave', geom='LINESTRING(189412 252431,189631 259122)'),
-         Road(name='Phil Tce', geom='LINESTRING(190131 224148,190871 228134)'),
-         Spot(name='test 1',geom='POINT(39.9130572 32.7892743)'),
-     ])
-
-session.commit()
+# session.add_all([
+#          Road(name='Jeff Rd', geom='LINESTRING(191232 243118,191108 243242)'),
+#          Road(name='Geordie Rd', geom='LINESTRING(189141 244158,189265 244817)'),
+#          Road(name='Paul St', geom='LINESTRING(192783 228138,192612 229814)'),
+#          Road(name='Graeme Ave', geom='LINESTRING(189412 252431,189631 259122)'),
+#          Road(name='Phil Tce', geom='LINESTRING(190131 224148,190871 228134)'),
+#          Spot(name='test 1',geom='POINT(39.9130572 32.7892743)'),
+#      ])
+#
+# session.commit()
 r2 = session.query(Road).filter_by(name='Paul St').first()
-
-print(r2.name,r2.geom)
-#print(session.scalar(Road.geom.srid))
 
 query = session.query(Road.name,
                       func.ST_Area(func.ST_Buffer(Road.geom, 2)) \
@@ -75,18 +64,17 @@ for row in query:
     print ('%s: %f' % (row.name, row.bufferarea))
 
 
-query = session.query(Spot.name,
+spotquery = session.query(Spot.name,
                       func.ST_X(Spot.geom).label('X'),func.ST_Y(Spot.geom).label('Y'))
-for row in query:
+spotCoordinates=[]
+for row in spotquery:
     print ('%s: %f  , %f ' % (row.name, row.X, row.Y))
+    spotCoordinates.append((row.X,row.Y))
 
+print(spotCoordinates)
+@appgeo.route("/")
+def mapview():
+      return render_template('indexgeo.html',spotCoordinates=spotCoordinates)
 
-
-
-
-# @appgeo.route("/")
-# def mapview():
-#     return render_template('indexgeo.html')
-#
-# if __name__ == "__main__":
-#     appgeo.run(debug=True, host="127.0.0.1",port=5012)
+if __name__ == "__main__":
+      appgeo.run(debug=True, host="127.0.0.1",port=5012)
